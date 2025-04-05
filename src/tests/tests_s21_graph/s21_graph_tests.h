88e0ab14db0b26b31475fa6ba7eb0342b795c4a4
montoyay <montoyay@student.21-school.ru>
2025-04-05 22:53:56 +0300

    montoyay [!feat] - add Makefile, create folders - lib and tests with two subfolders for two libraries, add main tests files

---
+++ src/tests/tests_s21_graph/s21_graph_tests.h
@@ -0,0 +1,8 @@
+#ifndef GRAPH_TESTS_H
+#define GRAPH_TESTS_H
+
+#include <gtest/gtest.h>
+
+// #include "..."
+
+#endif
---
+++ src/tests/tests_s21_graph_algorithms/s21_graph_algorithms_tests.h
@@ -0,0 +1,8 @@
+#ifndef GRAPH_ALGORITHMS_TESTS_H
+#define GRAPH_ALGORITHMS_TESTS_H
+
+#include <gtest/gtest.h>
+
+// #include "..."
+
+#endif
